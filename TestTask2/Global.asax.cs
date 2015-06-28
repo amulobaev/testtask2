34f2600cf43d4def4d3c531fee4da30138817946
Антон Мулобаев <devnull@localhost>
2015-06-28 23:11:10 +0500

    Добавил Ninject и другие компоненты

--- TestTask2/Global.asax.cs
+++ TestTask2/Global.asax.cs
@@ -4,18 +4,27 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Ninject;
+using Ninject.Web.Common;
 
 namespace TestTask2
 {
-    public class Global : HttpApplication
+    public class Global : NinjectHttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
+        protected override void OnApplicationStarted()
         {
+            base.OnApplicationStarted();
+
             // Код, выполняемый при запуске приложения
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected override IKernel CreateKernel()
+        {
+            return new StandardKernel(new MyNinjectModule());
         }
     }
 }
---
+++ TestTask2/MyNinjectModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Ninject.Modules;
+using TestTask2.Data.Repositories;
+
+namespace TestTask2
+{
+    public class MyNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IEquipmentRepository, EquipmentRepository>();
+        }
+    }
+}
